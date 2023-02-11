e5d024e86bca2a03a8ebd048ea42a69e13890e1f
vjprav <vj.prav18@gmail.com>
2023-02-11 17:41:33 -0500

    first build manual

--- hello_world.java
+++ hello_world.java
@@ -1,5 +1,5 @@
 public class hello_world {
   public static void main(String[] args) {
-    System.out.println("Trying webhooks now come on!");
+    System.out.println("Trying webhooks now come on! first build should be manual yes?");
   }
 }
