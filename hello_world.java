7e617cf42705f19afff982fec87c9e11061548ad
vjprav <vj.prav18@gmail.com>
2023-02-11 17:45:48 -0500

    Update hello_world.java

--- hello_world.java
+++ hello_world.java
@@ -1,5 +1,5 @@
 public class hello_world {
   public static void main(String[] args) {
-    System.out.println("Trying webhooks now come on! first build should be manual yes?");
+    System.out.println("webhook not working");
   }
 }
